55a198ff9bf4df8c504c594824d38f28d71d07fa
Sayan Mondal <smsayan09@gmail.com>
2026-05-20 00:10:44 +0530

    add signup auth

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         const {MONGO_URI} = process.env;
-        if(!MONGO_URI) throw new Error("MONGO_URI is not set");
+        if(!MONGO_URI) {
+            throw new Error("MONGO_URI is not set");
+        }
+        
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log("Mongo DB connected:", conn.connection.host)
     } catch (error) {
